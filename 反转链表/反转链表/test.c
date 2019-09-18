cc21a964ab70bad7baf0b7aa5d4618728f84731d
bushishanguu <1750169605@qq.com>
2019-09-18 12:46:01 +0800

    反转一个单链表

---
+++ 反转链表/反转链表/test.c
@@ -0,0 +1,28 @@
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     struct ListNode *next;
+* };
+*/
+
+
+struct ListNode* reverseList(struct ListNode* head){
+	if (head == NULL)
+	{
+		return head;
+	}
+	struct ListNode* pre = NULL;
+	struct ListNode* cur = head;
+	struct ListNode* next = NULL;
+	while (cur != NULL)
+	{
+		next = cur->next;
+		cur->next = pre;
+		pre = cur;
+		cur = next;
+	}
+	return pre;
+
+}
+
