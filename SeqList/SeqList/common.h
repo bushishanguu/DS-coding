2868d47164c2fe409126f6b472cd2c9e263f81e3
bushishanguu <1750169605@qq.com>
2019-10-16 23:21:37 +0800

    顺序表

---
+++ SeqList/SeqList/common.h
@@ -0,0 +1,28 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "assert.h"
+#include "stdbool.h"
+
+typedef enum{FALSE, TRUE} BOOL ;
+
+#define DataType int
+
+#define DEFAULT_SIZE 8
+#define INC_SIZE   3
+
+
+void Swap(DataType *a, DataType *b)
+{
+	DataType tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+
+
+
+
+#endif
